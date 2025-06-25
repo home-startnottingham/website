e4e1852aee18679d87a4aa5a2af4fbc310f28fd6
labiraus <oliver.hathaway@bjss.com>
2025-06-25 09:34:49 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#header').load("./header.html");
+  $('#carousel').load("./carousel.html");
+  console.log('xxxxx load carousel done');
+  $('#footer').load("./footer.html");
+});
+
+
+
